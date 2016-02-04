4e08a77dcca72534c45bb3313ea202481dc2347c
dpandya76 <dpandya76@gmail.com>
2016-02-04 18:30:17 -0500

    Added collision to asteroids and map
    Added sounds too..

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    // PUBLIC VARIABLE
+    public int asteroidNumber = 4;
+    public AsteroidController asteroids;
+
+	// Use this for initialization
+	void Start () {
+        this._initialize();
+       
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //private methods
+    private void _initialize()
+    {
+       for(int asteroidCount=0; asteroidCount<this.asteroidNumber;asteroidCount++)
+        {
+            Instantiate(asteroids.gameObject);
+        }
+
+    }
+}
---
+++ Assets/_Scripts/UFOCollider.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFOCollider : MonoBehaviour {
+
+   private AudioSource[] _audioSources;
+   private AudioSource _mapSound;
+   private AudioSource _asteroidSound;
+
+    //Public instance variables
+   public GameController gameController;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Initialize the audioSource array
+       this._audioSources = gameObject.GetComponents<AudioSource>();
+       this._mapSound = this._audioSources[2];
+       this._asteroidSound = this._audioSources[1];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Map"))
+        {
+            this._mapSound.Play();
+            //this.gameController.ScoreValue += 100;
+            //Debug.Log("Map Collision");
+        }
+
+        if (other.gameObject.CompareTag("Asteroid"))
+        {
+            this._asteroidSound.Play();
+            //this.gameController.LivesValue -= 1;
+            //Debug.Log("Asteroid Collision");
+        }
+    }
+}
