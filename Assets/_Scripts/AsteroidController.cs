eaca414b2a10421da7d68a150bc9b11edf5956e8
dpandya76 <dpandya76@gmail.com>
2016-02-04 16:56:32 -0500

    Added the asteroids
    The game has asteroids and also points generated randomly

---
+++ Assets/_Scripts/AsteroidController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidController : MonoBehaviour {
+
+    private Transform _transform;
+    public float minHorizontalSpeed = 5f;
+    public float maxHorizontalSpeed = 10f;
+    public float minVerticalSpeed = -2f;
+    public float maxVerticalSpeed = 2f;
+    private Vector2 _currentPosition;
+    private float _horizontalSpeed;
+    private float _verticalDrift;
+    // Use this for initialization
+    void Start()
+    {
+        //make a reference with the transform component
+        this._transform = gameObject.GetComponent<Transform>();
+        this.Reset();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector2(this._horizontalSpeed,this._verticalDrift);
+        this._transform.position = this._currentPosition;
+
+        if (this._currentPosition.x <= -355)
+        {
+            this.Reset();
+        }
+
+    }
+
+    public void Reset()
+    {
+        this._horizontalSpeed = Random.Range(this.minHorizontalSpeed, this.maxHorizontalSpeed);
+        this._verticalDrift = Random.Range(this.minVerticalSpeed, this.maxVerticalSpeed);
+        float yPosition = Random.Range(-207, 207);
+        this._transform.position = new Vector2(355f, yPosition);
+    }
+}
---
+++ Assets/_Scripts/MapController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour {
+
+    private Transform _transform;
+    public float speed = 5;
+    private Vector2 _currentPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        //make a reference with the transform component
+        this._transform = gameObject.GetComponent<Transform>();
+        this.Reset();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector2(this.speed,0);
+        this._transform.position = this._currentPosition;
+
+        if (this._currentPosition.x <= -345)
+        {
+            this.Reset();
+        }
+
+    }
+
+    public void Reset()
+    {
+        float yPosition = Random.Range(-214, 214);
+        this._transform.position = new Vector2(345f,yPosition);
+    }
+}
